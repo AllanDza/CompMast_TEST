7682ebb06d64963bc2625f34f906b7a173747232
Swathi-0 <105665956+Swathi-0@users.noreply.github.com>
2023-11-12 16:31:03 +0530

    Update maint.tf

--- terraform/maint.tf
+++ terraform/maint.tf
@@ -13,7 +13,7 @@ provider "aws" {
   region = var.region
 }
 resource "aws_instance" "servernode" {
-  ami                    = "ami-0287a05f0ef0e9d9a"
+  ami                    = "ami-0fc5d935ebf8bc3bc (64-bit (x86))"
   instance_type          = "t2.micro"
   key_name               = aws_key_pair.deployer.key_name
   vpc_security_group_ids = [aws_security_group.maingroup.id]
