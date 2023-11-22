12069a1b726f3f0cf782dce993bb22ca389beb8b
AllanDza <133175379+AllanDza@users.noreply.github.com>
2023-11-22 20:43:45 +0530

    Update maint.tf

--- terraform/maint.tf
+++ terraform/maint.tf
@@ -49,7 +49,7 @@ resource "aws_security_group" "maingroup" {
   ]
   ingress = [
     {
-      cidr_blocks      = ["0.0.0.0/0", ]
+      cidr_blocks      = ["0.0.0.0/0"]
       description      = ""
       from_port        = 22
       ipv6_cidr_blocks = []
@@ -60,7 +60,7 @@ resource "aws_security_group" "maingroup" {
       to_port          = 22
     },
     {
-      cidr_blocks      = ["0.0.0.0/0", ]
+      cidr_blocks      = ["0.0.0.0/0"]
       description      = ""
       from_port        = 80
       ipv6_cidr_blocks = []
